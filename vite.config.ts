3ad87aaedd8dfe4a13d5e85f3f3e7589b579bc8d
gayathri <gayathriraj.vlr@gmail.com>
2023-07-18 03:40:07 +0530

    misc changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
+  base: "",
+  plugins: [
+    react(), VitePWA({
     workbox: {
       globPatterns: ["**/*"],
     },
